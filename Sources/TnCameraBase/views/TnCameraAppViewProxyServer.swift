77a87870951887a1362a7a399c5358e5107154c9
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-12 14:10:54 +0700

    observabled

--- Sources/TnCameraBase/views/TnCameraAppViewProxyServer.swift
+++ Sources/TnCameraBase/views/TnCameraAppViewProxyServer.swift
@@ -10,21 +10,23 @@ import SwiftUI
 import TnIosBase
 
 public struct TnCameraAppViewProxyServer: TnCameraAppViewProtocol, TnLoggable {
+    public typealias TAppViewModel = TnCameraAppViewProxyServerModel
     public let LOG_NAME = "TnCameraAppViewProxyServer"
     
-    public typealias TAppViewModel = TnCameraAppViewProxyServerModel
     public var bottom: Optional<some View> {
         nil as EmptyView?
     }
     public var showToolbar: State<Bool> = .init(initialValue: true)
     
-    public var appModelState: StateObject<TAppViewModel>
-    public var appModel: TAppViewModel {
-        appModelState.wrappedValue
-    }
-    
-    public init(appModel: TAppViewModel) {
-        self.appModelState = .init(wrappedValue: appModel)
-        logDebug("inited")
-    }
+    //    public var appModelState: StateObject<TAppViewModel>
+    //    public var appModel: TAppViewModel {
+    //        appModelState.wrappedValue
+    //    }
+    //
+    //    public init(appModel: TAppViewModel) {
+    //        self.appModelState = .init(wrappedValue: appModel)
+    //        logDebug("inited")
+    //    }
+
+    @Binding public var appModel: TAppViewModel
 }
