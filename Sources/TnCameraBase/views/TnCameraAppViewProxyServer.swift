cc7347e138375613860043e6927690120f3f5fd9
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-12 14:56:33 +0700

    observabled

--- Sources/TnCameraBase/views/TnCameraAppViewProxyServer.swift
+++ Sources/TnCameraBase/views/TnCameraAppViewProxyServer.swift
@@ -9,28 +9,45 @@ import Foundation
 import SwiftUI
 import TnIosBase
 
-public struct TnCameraAppViewProxyServer: TnCameraAppViewProtocol, TnLoggable {
+public struct TnCameraAppViewProxyServer: TnLoggable {
     public typealias TAppViewModel = TnCameraAppViewProxyServerModel
     public let LOG_NAME = "TnCameraAppViewProxyServer"
     
     public var bottom: Optional<some View> {
         nil as EmptyView?
     }
-    public var showToolbar: State<Bool> = .init(initialValue: true)
     
-    //    public var appModelState: StateObject<TAppViewModel>
-    //    public var appModel: TAppViewModel {
-    //        appModelState.wrappedValue
-    //    }
-    //
-    //    public init(appModel: TAppViewModel) {
-    //        self.appModelState = .init(wrappedValue: appModel)
-    //        logDebug("inited")
-    //    }
-
-    public var appModel: StateObject<TAppViewModel>
+    @State var showToolbar = true
+    
+    @StateObject public var appModel: TAppViewModel
     public init(appModel: StateObject<TAppViewModel>) {
-        self.appModel = appModel
+        self._appModel = appModel
         logDebug("inited")
+    }
+        
+    public var body: some View {
+        ZStack {
+            if appModel.cameraModel.status == .started {
+                // preview
+                TnCameraPreviewViewMetal(imagePublisher: appModel.cameraManager.currentCiImagePublisher)
+                    .onTapGesture {
+                        withAnimation {
+                            showToolbar.toggle()
+                        }
+                    }
+                
+                // bottom toolbar
+                if showToolbar {
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        TnCameraToolbarMiscView(cameraManager: appModel.cameraManager)
+                        TnCameraToolbarMainView(cameraManager: appModel.cameraManager, bottom: bottom)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            appModel.setup()
+        }
     }
 }
