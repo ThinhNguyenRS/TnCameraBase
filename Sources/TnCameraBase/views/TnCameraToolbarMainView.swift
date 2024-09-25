65d5a2bb0f22b74d6e1e4afd382ab05143ebc380
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-25 15:35:31 +0700

    fixing async

--- Sources/TnCameraBase/views/TnCameraToolbarMainView.swift
+++ Sources/TnCameraBase/views/TnCameraToolbarMainView.swift
@@ -24,38 +24,36 @@ public struct TnCameraToolbarMainView<TBottom: View>: View, TnCameraViewProtocol
 
     public var body: some View {
         HStack {
-            if cameraModel.status == .started {
-                if let capturedImage = cameraModel.capturedImage {
-                    Image(uiImage: capturedImage)
-                        .tnMakeScalable()
-                        .frame(width: 80, height: 80)
-                }
-
-                Spacer()
-                circleButtonRotation(imageName: cameraModel.settings.cameraPosition.imageName) {
-                    cameraProxy.switchCamera()
-                }
-
-                Spacer()
-                getSettingsButton(type: .zoom, text: cameraModel.settings.zoomFactor.toString("%0.2f"))
-
-                if let bottom {
-                    Spacer()
-                    bottom
-                }
-
-                // capture
-                Spacer()
-                circleButtonRotation(imageName: "camera", radius: 90, backColor: .white, imageColor: .black) {
-                    cameraProxy.captureImage()
-                }
-
-                // settings
-                Spacer()
-                getSettingsButton(type: .misc, imageName: "ellipsis")
-
-                Spacer()
+            if let capturedImage = cameraModel.capturedImage {
+                Image(uiImage: capturedImage)
+                    .tnMakeScalable()
+                    .frame(width: 80, height: 80)
             }
+
+            Spacer()
+            circleButtonRotation(imageName: cameraModel.settings.cameraPosition.imageName) {
+                cameraProxy.switchCamera()
+            }
+
+            Spacer()
+            getSettingsButton(type: .zoom, text: cameraModel.settings.zoomFactor.toString("%0.2f"))
+
+            if let bottom {
+                Spacer()
+                bottom
+            }
+
+            // capture
+            Spacer()
+            circleButtonRotation(imageName: "camera", radius: 90, backColor: .white, imageColor: .black) {
+                cameraProxy.captureImage()
+            }
+
+            // settings
+            Spacer()
+            getSettingsButton(type: .misc, imageName: "ellipsis")
+
+            Spacer()
         }
     }
 }
