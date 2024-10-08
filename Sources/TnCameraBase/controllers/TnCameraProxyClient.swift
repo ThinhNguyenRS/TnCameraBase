0347cae50034bc2b6f6fc24042c9d44e46baeec8
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-10-08 20:32:27 +0700

    cleanup model

--- Sources/TnCameraBase/controllers/TnCameraProxyClient.swift
+++ Sources/TnCameraBase/controllers/TnCameraProxyClient.swift
@@ -199,7 +199,7 @@ extension TnCameraProxyClient: TnCameraProxyProtocol {
     }
 
     public func send(_ object: TnCameraMessageProtocol, useBle: Bool = false) {
-        if useBle {
+        if useBle || network == nil {
             try? ble.send(object: object)
         } else {
             try? network?.send(object: object)
