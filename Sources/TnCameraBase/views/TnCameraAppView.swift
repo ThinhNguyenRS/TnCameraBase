8cd472aeac46cfd2d17a1c5625979f40ff1e38e1
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-10-10 07:52:00 +0700

    refactor app view

--- Sources/TnCameraBase/views/TnCameraAppView.swift
+++ Sources/TnCameraBase/views/TnCameraAppView.swift
@@ -10,157 +10,6 @@ import SwiftUI
 import TnIosBase
 import CoreData
 
-// MARK: TnCameraAppViewDelegate
-protocol TnCameraAppViewDelegate {
-    func onChanged(status: TnCameraStatus, settings: TnCameraSettings)
-}
-
-// MARK: TnCameraAppViewInternal
-struct TnCameraAppViewInternal: View, TnLoggable {
-    @State private var showToolbar = false
-    @State private var toolbarType: TnCameraToolbarViewType = .main
-    @State private var settings: TnCameraSettings = .init()
-    @State private var status: TnCameraStatus = .none
-    @State private var capturedImage: UIImage? = nil
-    
-    private let delegate: TnCameraAppViewDelegate?
-    
-    init(delegate: TnCameraAppViewDelegate? = nil) {
-        self.delegate = delegate
-        globalCameraProxy.delegate = self
-        logDebug("inited")
-    }
-    
-    var body: some View {
-        ZStack {
-            // background
-            Rectangle()
-                .fill(.black)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-
-            if status == .started {
-                // preview
-                TnCameraPreviewViewMetal(imagePublisher: { await cameraProxy.currentCiImagePublisher })
-                    .onTapGesture {
-                        withAnimation {
-                            showToolbar.toggle()
-                        }
-                    }
-                // toolbar
-                TnCameraToolbarView(
-                    showToolbar: $showToolbar,
-                    toolbarType: $toolbarType,
-                    settings: $settings,
-                    capturedImage: $capturedImage
-                )
-            }
-        }
-        .overlay(alignment: .top) {
-            TnCameraToolbarTopView()
-        }
-        .onAppear {
-            logDebug("appear")
-        }
-    }
-}
-
-extension TnCameraAppViewInternal: TnCameraDelegate {
-    public func tnCamera(captured: TnCameraPhotoOutput) {
-        capturedImage = UIImage(data: captured.photoData)
-    }
-    
-    public func tnCamera(status: TnCameraStatus) {
-        guard self.status != status else { return }
-        
-        DispatchQueue.main.async {
-            logDebug("status changed", status)
-            self.status = status
-        }
-        delegate?.onChanged(status: status, settings: settings)
-    }
-    
-    public func tnCamera(settings: TnCameraSettings) {
-        DispatchQueue.main.async {
-            logDebug("settings changed")
-            self.settings = settings
-        }
-        delegate?.onChanged(status: status, settings: settings)
-    }
-}
-
-// MARK: TnCameraAppViewMaster
-@available(iOS 17.0, *)
-public struct TnCameraAppViewMaster: View, TnLoggable {
-    @State private var cameraSettingsID: NSManagedObjectID = .init()
-
-    public init(EOM: String? = nil, MTU: Int? = nil, encoder: TnEncoder, decoder: TnDecoder) {
-        let cameraProxy = TnCameraProxyServerAsync(
-            TnCameraService.shared,
-            bleInfo: TnCameraProxyServiceInfo.getBle(),
-            transportingInfo: TnCameraProxyServiceInfo.getTransporting(EOM: EOM, MTU: MTU, encoder: encoder, decoder: decoder)
-        )
-        cameraProxy.bleDelegate = cameraProxy
-        globalCameraProxy = cameraProxy
-        logDebug("inited")
-    }
-    
-    public var body: some View {
-        TnCameraAppViewInternal(delegate: self)
-            .task {
-                try? await tnDoCatchAsync(name: "TnCameraAppViewMaster setup") {
-                    logDebug("setup ...")
-
-                    let settingsPair = try await TnCodablePersistenceController.shared.fetch(defaultObject: { TnCameraSettings.init() })
-                    cameraSettingsID = settingsPair.objectID
-                    await TnCameraService.shared.setSettings(settings: settingsPair.object)
-                    
-                    globalCameraProxy.setup()
-
-                    logDebug("setup !")
-                }
-            }
-    }
-}
-
-@available(iOS 17.0, *)
-extension TnCameraAppViewMaster: TnCameraAppViewDelegate {
-    func onChanged(status: TnCameraStatus, settings: TnCameraSettings) {
-        logDebug("send settings")
-        cameraProxy.send(.getSettingsResponse, TnCameraSettingsValue(settings: settings, status: status))
-        
-//        Task {
-//            logDebug("save settings")
-//            try? await TnCodablePersistenceController.shared.update(
-//                objectID: cameraSettingsID,
-//                object: settings
-//            )
-//        }
-    }
-}
-
-// MARK: TnCameraAppViewSlaver
-public struct TnCameraAppViewSlaver: View, TnLoggable {
-    public init(EOM: String? = nil, MTU: Int? = nil, encoder: TnEncoder, decoder: TnDecoder) {
-        let cameraProxy = TnCameraProxyClient(
-            bleInfo: TnCameraProxyServiceInfo.getBle(),
-            transportingInfo: TnCameraProxyServiceInfo.getTransporting(EOM: EOM, MTU: MTU, encoder: encoder, decoder: decoder)
-        )
-        cameraProxy.bleDelegate = cameraProxy
-        globalCameraProxy = cameraProxy
-        globalCameraProxy.setup()
-    }
-    
-    public var body: some View {
-        TnCameraAppViewInternal(delegate: self)
-    }
-}
-
-extension TnCameraAppViewSlaver: TnCameraAppViewDelegate {
-    func onChanged(status: TnCameraStatus, settings: TnCameraSettings) {
-    }
-}
-
-
 // MARK: TnCameraAppView
 public struct TnCameraAppView: View, TnLoggable {
     @State private var showToolbar = false
@@ -173,10 +22,12 @@ public struct TnCameraAppView: View, TnLoggable {
     private let bleInfo: TnNetworkBleInfo
     private let transportingInfo: TnNetworkTransportingInfo
 
-    public init(serverMode: Bool, EOM: String? = nil, MTU: Int? = nil, encoder: TnEncoder, decoder: TnDecoder) {
+    public init(serverMode: Bool, bleInfo: TnNetworkBleInfo, transportingInfo: TnNetworkTransportingInfo) {
         self.serverMode = serverMode
-        bleInfo = TnCameraProxyServiceInfo.getBle()
-        transportingInfo = TnCameraProxyServiceInfo.getTransporting(EOM: EOM, MTU: MTU, encoder: encoder, decoder: decoder)
+        self.bleInfo = bleInfo
+        self.transportingInfo = transportingInfo
+//        bleInfo = TnCameraProxyServiceInfo.getBle()
+//        transportingInfo = TnCameraProxyServiceInfo.getTransporting(EOM: EOM, MTU: MTU, encoder: encoder, decoder: decoder)
         logDebug("inited")
     }
     
---
+++ Sources/TnCameraBase/views/TnCameraAppViewSlaver.swift
@@ -0,0 +1,32 @@
+//
+//  TnCameraAppViewSlaver.swift
+//  TnCameraBase
+//
+//  Created by Thinh Nguyen on 10/10/24.
+//
+
+import Foundation
+import SwiftUI
+import TnIosBase
+
+// MARK: TnCameraAppViewSlaver
+public struct TnCameraAppViewSlaver: View, TnLoggable {
+    public init(EOM: String? = nil, MTU: Int? = nil, encoder: TnEncoder, decoder: TnDecoder) {
+        let cameraProxy = TnCameraProxyClient(
+            bleInfo: TnCameraProxyServiceInfo.getBle(),
+            transportingInfo: TnCameraProxyServiceInfo.getTransporting(EOM: EOM, MTU: MTU, encoder: encoder, decoder: decoder)
+        )
+        cameraProxy.bleDelegate = cameraProxy
+        globalCameraProxy = cameraProxy
+        globalCameraProxy.setup()
+    }
+    
+    public var body: some View {
+        TnCameraAppViewInternal(delegate: self)
+    }
+}
+
+extension TnCameraAppViewSlaver: TnCameraAppViewDelegate {
+    func onChanged(status: TnCameraStatus, settings: TnCameraSettings) {
+    }
+}
